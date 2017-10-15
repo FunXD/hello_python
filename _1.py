497ac70da7e6f4ffdfcd67cfc241de91519ca4d9
here FunXD has come! <united_7@hanmail.net>
2017-10-15 10:39:23 +0900

    0-open Repository

---
+++ _1.py
@@ -0,0 +1,72 @@
+"""
+convention : 전통
+----
+1. file name, variable = _=temporary, __=내장함수, a, B(x), 1(x) ,&(x)
+2. 1-line = 80 charactors, (=recommandation)
+3. Indent = 들여쓰기 (4칸 = space), Atom Tab = 4 Spaces
+    혼용해서 쓰지 말것
+"""
+#comment
+
+a = 300
+b = 333
+c = a * b
+
+print(c)
+
+
+"""---- for 루프"""
+
+for n in range(5):
+    print("Hello python")
+print("END")
+
+""" If문 """
+if n >=5:
+    print('greater than 5..')
+    print('*****')
+else:
+    print ('less than 5..')
+    print ('*****')
+print('END')
+
+print('n=', n)
+
+
+""" (3)---- 함수: (definition : 정의)"""
+def a_add_b(a,b)
+    pass
+
+#함수 정의
+def a_add_b(a,b):
+
+#메쏘드 정의
+def _a_add_b(a,b):
+
+
+_a_add_b
+
+
+
+answer = a_add_b(3,4)
+print (answer)
+
+
+print('     *     ')
+print('    ***    ')
+print('   *****   ')
+print('  *******  ')
+print(' ********* ')
+print('***********')
+print('    ㅣㅣ    ')
+print('    ㅣㅣ   ')
+print('mmmmmmmmmmm')
+
+
+print('STAR'.center(31))
+print('BUCKS'.center(31))
+for m in range(1, 7):
+    print(" "*(15-m), '*'*(2*m-1))
+for k in range(3):
+    print("|".center(31))
+print('M'*31)
